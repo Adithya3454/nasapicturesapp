43676a8e8ef430d1c337dea4c218ebd8560f86c0
adithyasirigeri <adithya3454@bitbucket.org>
2020-02-15 20:33:53 +0530

    Added interfaces to NasaPicturesMainContract that deal with loading data and presenting views.

--- app/src/main/java/com/nasapicturesapp/contracts/NasaPicturesMainContract.java
+++ app/src/main/java/com/nasapicturesapp/contracts/NasaPicturesMainContract.java
@@ -1,4 +1,32 @@
 package com.nasapicturesapp.contracts;
 
+import com.nasapicturesapp.model.NasaPicture;
+
+import java.util.List;
+
 public class NasaPicturesMainContract {
+
+    public interface NasaPicturesView{
+
+        void showProgress();
+
+        void hideProgress();
+    }
+
+    public interface NasaPicturesPresenter {
+
+        void loadNasaPicturesGallery();
+
+        void onDestroy();
+    }
+
+    public interface GetNasaPicturesInteractor {
+
+        interface NasaPicturesLoadingFinishedListener{
+            void onFinishedLoading(List<NasaPicture> nasaPictureList);
+        }
+
+        void getPicturesForNasaGallery(NasaPicturesLoadingFinishedListener nasaPicturesLoadingFinishedListener);
+    }
+
 }
